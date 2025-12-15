8330ba9c589d73a081387f911d0449a57a328c7c
amithjo001 <amith98gogle@gmail.com>
2025-12-15 13:16:17 +0500

    Fix Service Unavailable by removing python.py and fixing Flask app

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "App Engine Flask App is running successfully!"
+    return "App Engine CI/CD is working successfully 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
