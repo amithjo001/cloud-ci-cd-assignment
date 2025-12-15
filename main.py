8f7f9b5fb2f203c2a01ca9f43238b79448673957
amithjo001 <amith98gogle@gmail.com>
2025-12-15 13:03:50 +0500

    Fix App Engine config and Flask entrypoint

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Cloud CI/CD Assignment – App Engine Deployment Successful"
+    return "App Engine Flask App is running successfully!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
